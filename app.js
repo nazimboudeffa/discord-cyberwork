29d54bc7d11ba8dd6926fff20c09b24022b7a3ae
Nazim Boudeffa <boudeffa@gmail.com>
2020-05-25 10:57:30 +0200

    Added request promise

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const express = require('express')
 const app = express()
 const ejs = require('ejs')
 const path = require('path')
+const rp = require('request-promise');
 
 const Work = require('./work.js');
 
@@ -32,8 +33,9 @@ app.listen(port, function () {
 const prefix = 'w!';
 
 let work;
-let language = 'javascript'
+let language = 'javascript';
 let userJoined = false;
+let workChannel = 'work-work';
 
 /* User joins the server */
 client.on('message', (message) => {
@@ -43,34 +45,72 @@ client.on('message', (message) => {
     const args = message.content.slice(prefix.length).split('!');
     const command = args.shift().toLowerCase();
 
-    if(command === 'join'){
+    var actualChannel = message.channel.name
 
-      work = new Work(message.author.username, client, 'work-work'); //Initilizes our work engine
-      let myRet = work.startWork(language);
-      const embed = new Discord.MessageEmbed()
-      .setTitle("Welcome to Let's Code Better Together")
-      .setColor(0xFF0000)
-      .addField(`${message.author.username} has Joined`, myRet);
-      message.channel.send(embed);
-      message.channel.send(`${message.author} type w!commands to see the list of commands.`);
-      userJoined = true;
-      return;
+    if(actualChannel != workChannel) {
+        message.channel.send(`${message.author} you cannot use a w! command here, please work in #${workChannel}`);
+    } else {
 
-    }
+      if(command === 'join'){
 
-    if(userJoined == true){
-        if(command === 'issue'){
-          //message.channel.send(work.getIssue(language));
-          let issue = work.getIssue(language);
-          const embed = new Discord.MessageEmbed()
-          .setTitle(issue.title)
-          .setColor(0xFF0000)
-          .addField(`URL`, issue.html_url);
-          message.channel.send(embed);
-        }
-        else if(command === 'stats'){
-          message.channel.send(work.getStats());
-        }
+        work = new Work(message.author.username, client, workChannel, language); //Initilizes our work engine
+        let myRet = work.startWork();
+        const embed = new Discord.MessageEmbed()
+        .setTitle("Welcome to Let's Code Better Together")
+        .setColor(0xFF0000)
+        .addField(`${message.author.username} has joined`, myRet);
+        message.channel.send(embed);
+        message.channel.send(`${message.author} type w!commands to see the list of commands.`);
+        userJoined = true;
+        return;
+
+      }
+
+
+      if(userJoined == true){
+          if(command === 'issue'){
+
+            var random_number = function (x) {
+                return Math.random(x);
+            };
+
+            var options = {
+              url: 'https://api.github.com/search/issues?q=language:' + encodeURIComponent(work.language) + '+state:open',
+              headers: {
+                'User-Agent': 'request'
+              }
+            };
+
+            rp(options, function (error, response, body) {
+               if (!error && response.statusCode === 200) {
+
+                 //TODO understand why it doesn't work when I pass it the the Work class
+
+                 const embed = new Discord.MessageEmbed()
+                 .setTitle(JSON.parse(body).items[0].title)
+                 .setColor(0xFF0000)
+                 .addField(`URL`, JSON.parse(body).items[0].html_url);
+                 message.channel.send(embed);
+
+               }
+            })
+
+          }
+          else if(command === 'stats'){
+            var options = {
+              url: 'https://api.github.com/search/issues?q=language:' + encodeURIComponent(work.language) + '+state:open',
+              headers: {
+                'User-Agent': 'request'
+              }
+            };
+
+            rp(options, function (error, response, body) {
+               if (!error && response.statusCode === 200) {
+                 message.channel.send(work.getStats(body));
+               }
+            })
+          }
+      }
     }
 });
 
--- work.js
+++ work.js
@@ -1,57 +1,44 @@
-const request = require("request");
-
+const rp = require('request-promise');
 class Work {
-    constructor(worker, client, channelName='work-test'){
+    constructor(worker, client, channelName='work-test', language){
+
          this.client = client;
          this.channelName = channelName;
          this.currentWorker = worker;
-         this.total_count = 0;
-         this.issue = '';
+         this.language = language;
+
     };
 
-    startWork(language){
-      var options = {
-        url: 'https://api.github.com/search/issues?q=language:' + encodeURIComponent(language) + '+state:open',
-        headers: {
-          'User-Agent': 'request'
-        }
-      };
-      request(options, function (error, response, data) {
-        this.issues = JSON.parse(data).total_count;
-        console.log(this.total_count);
-      });
-      return [`Number of Issues: ${this.total_count}\n`];
+    startWork(){
+      return [`Language: ${this.language}\n`];
     }
 
-    /* main menu */
-    getIssue(language){
+    getIssue(){
 
+      var issue;
       var random_number = function (min, max) {
           return Math.floor(Math.random() * (max - min + 1)) + min;
       };
 
-      var data;
-      var issue;
-      var counter = random_number(0, this.total_count);
+      var options = {
+        url: 'https://api.github.com/search/issues?q=language:' + encodeURIComponent(this.language) + '+state:open',
+        headers: {
+          'User-Agent': 'request'
+        }
+      };
 
-      if ((counter > (this.issues - 1)) || (counter === 0)) {
-        var options = {
-          url: 'https://api.github.com/search/issues?q=language:' + encodeURIComponent(language) + '+state:open',
-          headers: {
-            'User-Agent': 'request'
-          }
-        };
-        request(options, function (error, response, data) {
-          this.issue = JSON.parse(data).items[counter];
-          console.log(this.issue);
-        });
-        return [`Solve: ${this.issue.html_url}\n`];
-      }
+      rp(options, function (error, response, body) {
+         if (!error && response.statusCode === 200) {
+           return(JSON.parse(body).items[0]);
+         }
+      })
+
+      console.log(issue);
 
     }
 
-    getStats(){
-      return [`Your Issue: ${this.issue}\n`];
+    getStats(data){
+      return [`Issues in ${this.language}: ${JSON.parse(data).total_count}\n`];
     }
 }
 module.exports = Work;
